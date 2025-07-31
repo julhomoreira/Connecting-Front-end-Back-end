8cf021df74d72b02f9ad4031e0fca636d899b9c2
Julho César Moreira <julhomoreira1988@gmail.com>
2025-07-31 18:23:38 +0100

    Added React/Node
    Working with react...

---
+++ BackEnd/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json())
+
+const users = [
+    {
+        name: 'Rodollfo',
+        age: 29
+    },
+]
+
+// aqui estou  recuperando os usuarios, requerendo atráves do GET
+app.get('/usuarios', function(request, response){  
+    response.json(users);
+})
+
+
+// aqui estou criando novos usuarios atráves do POST
+app.post('/usuarios', function(request, response){
+    console.log(request.body)
+
+    const newUser = request.body
+
+    users.push(newUser)
+
+    response.status(201).json(newUser)
+})
+
+//aqui tenho a porta do meu servidor rodando
+app.listen(30001, () => console.log("servidor rodando"));
+
